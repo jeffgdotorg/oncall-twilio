9858de1217ff6cf24a1add422cbcd02a74bdba1b
Jeff Gehlbach <jeffg@opennms.com>
2020-05-29 16:29:37 +0000

    - Comments regarding purpose of .env.example files - Switched make_call.py to use python-dotenv

--- app/make_call.py
+++ app/make_call.py
@@ -1,12 +1,14 @@
+import os
+from dotenv import load_dotenv
 from twilio.rest import Client
-from os import environ
 
-client = Client(environ['TWILIO_ACCOUNT_SID'], environ['TWILIO_AUTH_TOKEN'])
+load_dotenv()
+client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to=environ['ONMS_DEFAULT_TARGET_NUMBER'],
-                        from_=environ['ONMS_DEFAULT_SOURCE_NUMBER']
+                        to=os.getenv("ONMS_DEFAULT_TARGET_NUMBER"),
+                        from_=os.getenv("ONMS_DEFAULT_SOURCE_NUMBER")
                     )
 
 print(call.sid)
